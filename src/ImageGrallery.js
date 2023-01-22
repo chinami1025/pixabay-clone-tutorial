86f4af25b138fa020c7c7f28d7458002d8efdc9d
chinami1025 <toda.chinami@gmail.com>
2023-01-22 23:42:23 +0900

    First commit

---
+++ src/ImageGrallery.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./ImageGralley.css";
+
+const ImageGrallery = () => {
+  return (
+    <div>
+      <div className="images-wrapper">
+        <div className="image">
+          <img src="" alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ImageGrallery;
